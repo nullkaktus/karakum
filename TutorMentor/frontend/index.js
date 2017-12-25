fb91e761ef1a759c1bd69c766b0f729e61285d47
Diana <deanna@Deanna.local>
2017-12-25 17:09:20 +0100

    Old files and high-fidelity mockups

---
+++ TutorMentor/frontend/index.js
@@ -0,0 +1,15 @@
+angular.module("App", []);
+/*
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+  .state('first', {
+      url: "/first",
+      templateUrl: "templates/first.html",
+      controller: 'FirstCtrl'
+    })
+})
+.controller('FirstCtrl', function($scope) {
+  console.log('FirstCtrl');
+    $scope.name = "Something";
+});
+*/
